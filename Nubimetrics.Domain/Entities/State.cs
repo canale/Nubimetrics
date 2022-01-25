af4c04358cd4693eaf6ceab923315293753c2108
canale <canalejandro@gmail.com>
2022-01-25 14:31:41 -0300

    Adds State Entity for Pais

---
+++ Nubimetrics.Domain/Entities/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nubimetrics.Domain.Entities
+{
+    public class State : Entity<string>
+    {
+        public string Name { get; }
+
+        public State(string id, string name ):base(id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' can't be null nor empty.", nameof(id));
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException($"'{nameof(name)}' can't be null nor empty.", nameof(name));
+            }
+
+            Name = name;
+        }
+    }
+}
