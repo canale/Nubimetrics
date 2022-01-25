cff82c5e590dbdd34d944f3ac60cfd91e61a0e35
canale <canalejandro@gmail.com>
2022-01-25 14:30:36 -0300

    Adds support for Entities

---
+++ Nubimetrics.Domain/Entities/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Nubimetrics.Domain.Entities
+{
+    public abstract class Entity<TId>
+    {
+        TId Id { get;  }
+
+        public Entity(TId id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var other = (Entity<TId>)obj;
+            return this.Id.Equals(other.Id);
+        }
+
+        public static bool operator ==( Entity<TId> one,  Entity<TId> two)
+            => one?.Equals(two) ?? false;
+
+        public static bool operator !=( Entity<TId> one,  Entity<TId> two)
+            => !(one?.Equals(two) ?? false);
+
+        public override int GetHashCode() 
+            => this.Id.GetHashCode();
+    }
+}
