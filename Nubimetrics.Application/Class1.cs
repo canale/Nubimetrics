2418a74e8f736b6b6246a2b96cf93f7163e8cd4d
canale <canalejandro@gmail.com>
2022-01-25 14:30:17 -0300

    Adds support for ValueObjects

--- Nubimetrics.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Nubimetrics.Application
-{
-    public class Class1
-    {
-    }
-}
--- Nubimetrics.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Nubimetrics.Domain
-{
-    public class Class1
-    {
-    }
-}
