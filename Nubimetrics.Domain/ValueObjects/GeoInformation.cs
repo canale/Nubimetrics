de068545c17003916d1bcdd8b5ddcf27c2ef9f2a
canale <canalejandro@gmail.com>
2022-01-25 14:31:18 -0300

    Adds GeoInformation ValueObjects for Pais entity

---
+++ Nubimetrics.Domain/ValueObjects/GeoInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nubimetrics.Domain.ValueObjects
+{
+    public class GeoInformation : ValueObject
+    {
+        public Location Location { get; }
+
+        public GeoInformation(Location location)
+        {
+            if (location is null)
+            {
+                throw new ArgumentNullException(nameof(location));
+            }
+            Location = location;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Location;
+        }
+    }
+}
---
+++ Nubimetrics.Domain/ValueObjects/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nubimetrics.Domain.ValueObjects
+{
+    public class Location : ValueObject
+    {
+        public decimal Latitude { get;}
+        public decimal Longitude { get;}
+
+        public Location(decimal latitude, decimal longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Latitude;
+            yield return Longitude;
+        }
+    }
+}
