595446a8624dc3b2be5632aab2be8bda155c8cf5
canale <canalejandro@gmail.com>
2022-01-26 18:49:16 -0300

    Adds json trasformation policy Form camel case to underscore

---
+++ Nuimetrics.DataAccess/Helpers/UpperCaseNamingPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace Nubimetrics.DataAccess.Helpers
+{
+    internal class UpperCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            Regex rx = new Regex(@"([A-Z])");
+            string result = rx.Replace(name, new MatchEvaluator(CapText));
+            return result;
+        }
+
+        static string CapText(Match m)
+        {
+            string x = m.ToString();
+            return m.Index == 0 ? char.ToLower(x[0]).ToString() : $"_{char.ToLower(x[0])}";
+        }
+    }
+}
